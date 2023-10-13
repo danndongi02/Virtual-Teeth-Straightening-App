cd97701875e873769ee7db830d4e75bb6a29cbee
danndongi02 <ianndunguh@gmail.com>
2023-10-13 20:52:17 +0300

    Removed sample logger script

--- logger.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logging.basicConfig(filename='logs/logs2.log', encoding='utf-8', level=logging.INFO)
-logging.info("This is a sample log message for info")
-logging.error("This is a sample log message for error")
