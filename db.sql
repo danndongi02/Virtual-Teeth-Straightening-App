41db6cd586eed6f88d970cecadc92de1be7e61f0
danndongi02 <ianndunguh@gmail.com>
2023-11-09 04:39:30 +0300

    Added new queries

--- db.sql
+++ db.sql
@@ -1,4 +1,9 @@
 -- Create VTSB database
 CREATE DATABASE vtsb_db;
 
-SELECT * FROM vtsb_db;
+SELECT * FROM conversations;
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public'
+ORDER BY table_name;
