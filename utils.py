7ed6e3671b3c8f366c6a76ffa800e158a91e5f6b
danndongi02 <ianndunguh@gmail.com>
2023-11-24 01:35:02 +0300

    Added store_conversation function

--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@ import logging
 
 from twilio.rest import Client
 from decouple import config
+from sqlalchemy.exc import SQLAlchemyError
+
+# internal imports
+from models import Conversation
 
 
 # set up twilio credentials
@@ -31,3 +35,21 @@ def send_message(to_number, body_text):
     except Exception as e:
         logger.error(f"Error sending message to {to_number}: {e}")
         print("Messange not sent")
+        
+        
+# store conversation in database
+def store_conversation(db, whatsapp_number, body_text, response_text):
+    try:
+        conversation = Conversation(
+            sender=whatsapp_number,
+            message=body_text,
+            response=response_text
+        )
+        
+        db.add(conversation)
+        db.commit()
+        logger.info(f"Conversation #{conversation.id} stored in database")
+        
+    except SQLAlchemyError as e:
+        db.rollback()
+        logger.error(f"Error storing conversation in database: {e}")
